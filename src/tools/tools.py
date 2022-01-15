3aa0135f2ed61e72111b44660ded4383afe4979b
Paul <pawelpelar@gmail.com>
2022-01-15 12:39:01 +0100

    final greyscale model

--- src/tools/tools.py
+++ src/tools/tools.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 import numpy as np
@@ -9,6 +10,9 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
 from tensorflow.keras.layers import Dense,\
      Activation, Flatten, Conv2D, MaxPooling2D
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_fscore_support
+from keras import backend as K
 
 
 class prepareData:
@@ -19,7 +23,14 @@ class prepareData:
         self.cwd = os.getcwd()
         self.data_dir = os.path.join(self.cwd, 'src', 'data', 'full_data')
         self.test_data_dir = os.path.join(self.cwd, 'src', 'test_data')
-        self.categories = ['Basil', 'Chinar', 'Jatropha', 'Mango', 'Pongamia']
+        self.model_dir = os.path.join(self.cwd, 'src', 'models')
+        self.categories = [
+            'Alstonia Scholaris', 'Arjun', 'Basil', 'Chinar', 'Gauva', 'Jamun',
+            'Jatropha', 'Lemon', 'Mango', 'Pomegranate', 'Pongamia Pinnata'
+            ]
+        self.categories = [
+            'Alstonia Scholaris', 'Pongamia Pinnata'
+            ]
 
         self.img_size_x = 60
         self.img_size_y = 40
@@ -30,17 +41,39 @@ class prepareData:
         self.train_X = []
         self.train_y = []
 
+        self.test_X = []
+        self.test_y = []
+
         # model
         self.loss = 'categorical_crossentropy'
         self.optimizer = 'adam'
 
     def main(self):
+        self.greyscale_model()
+        
+    def greyscale_model(self):
         self.prepare_data()
         self.shuffle_data()
         self.neural_network_prep()
-        # self.normalize_data()
-        self.cnn_model = self.create_KNN_model()
-        self.predict()
+        self.cnn_model = self.create_CNN2_model()
+
+        predictions = self.cnn_model.predict(self.test_X)
+        classes = np.argmax(predictions, axis = 1)
+        precision, recall, fbeta, support = precision_recall_fscore_support(self.test_y, classes)
+
+        filename = os.path.join(self.model_dir, 'color_model')
+
+        self.cnn_model.save(filename)
+
+        data = {
+            'precision': precision.tolist(),
+            'recall': recall.tolist(),
+            'fbeta': fbeta.tolist(),
+            'support': support.tolist()
+        }
+
+        with open(filename + '.json', 'w') as jf:
+            json.dump(data, jf, indent=4)
 
     def prepare_data(self):
         self.logger.info("Preparing data...")
@@ -49,6 +82,8 @@ class prepareData:
             class_num = self.categories.index(category)
             if os.path.exists(data_path):
                 for img in os.listdir(data_path):
+                    if '.DS_Store' in img:
+                        continue
                     img_path = os.path.join(data_path, img)
                     try:
                         img_array = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
@@ -60,7 +95,8 @@ class prepareData:
                         self.logger.error(
                             f"Broken or none image file: {img_path}")
             else:
-                self.logger.warning("Incorrect path to data")
+                self.logger.warning(f"Incorrect path to data. path:{data_path}")
+                
 
         if self.show_example:
             plt.imshow(resized_array, cmap="gray")
@@ -73,7 +109,10 @@ class prepareData:
 
     def neural_network_prep(self):
         self.logger.info("Creating features and labels...")
-        for features, label in self.training_data:
+        train, test =  train_test_split(self.training_data, test_size=0.2, random_state=25)
+
+        
+        for features, label in train:
             self.train_X.append(features)
             self.train_y.append(label)
 
@@ -81,43 +120,16 @@ class prepareData:
             self.train_X).reshape(-1, self.img_size_x, self.img_size_y, 1)
         self.train_y = np.array(self.train_y)
 
-    def normalize_data(self):
-        self.logger.info("Normalizing data...")
-        self.train_X = self.train_X/255.0
+        for features, label in test:
+            self.test_X.append(features)
+            self.test_y.append(label)
 
-    def create_CNN_model(self):
-        self.logger.info("Creating the model...")
-        model = Sequential()
-
-        model.add(Conv2D(64, 5, input_shape=self.train_X.shape[1:]))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        model.add(Conv2D(64, 5))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        model.add(Flatten())
-        model.add(Dense(64, input_dim=5))
-        model.add(Activation("relu"))
-
-        model.add(Dense(1)) # jedna wartosc zwracana
-        model.add(Activaton('softmax')) 
-
-        model.compile(
-            loss=self.loss,
-            optimizer=self.optimizer,
-            metrics=['accuracy'])
-
-        model.fit(
-            self.train_X, self.train_y,
-            batch_size=32, validation_split=0.1,
-            epochs=5)
-
-        return model
+        self.test_X = np.array(
+            self.train_X).reshape(-1, self.img_size_x, self.img_size_y, 1)
+        self.test_y = np.array(self.train_y)
 
     def create_CNN2_model(self):
-        num_classes = 5
+        num_classes = len(self.categories)
 
         model = Sequential([
             layers.Rescaling(1./255, input_shape=self.train_X.shape[1:]),
@@ -130,13 +142,13 @@ class prepareData:
             layers.Flatten(),
             layers.Dense(128, activation='relu'),
             layers.Dense(num_classes)
-        ])
+        ])  
 
         model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+              metrics=['acc'])
         
-        model.fit(
+        self.history = model.fit(
             self.train_X, self.train_y,
             batch_size=32, validation_split=0.1,
             epochs=30)
@@ -159,7 +171,7 @@ class prepareData:
                     print(f'PREDICTION OF {img}: {plant_prediction}')
 
                 except Exception as err:
-                    self.logger.error(f"Broken or none image file: {img_path}")
-                    print(err)
+                    self.logger.error(err)
+                    
         else:
             self.logger.warning("Incorrect path to data")
